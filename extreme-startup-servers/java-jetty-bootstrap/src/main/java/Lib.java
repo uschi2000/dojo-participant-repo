bc11a165cc27b2ece9729ccc4a93218917299d8e
Robert Fink <rfink@palantir.com>
2016-04-08 17:25:40 -0700

    add cube sqrt

--- extreme-startup-servers/java-jetty-bootstrap/src/main/java/Lib.java
+++ extreme-startup-servers/java-jetty-bootstrap/src/main/java/Lib.java
@@ -15,4 +15,21 @@ public class Lib {
         return Iterables.transform(ints, Integer::parseInt);
     }
 
+    public static int multiply(Iterable<Integer> ints) {
+        int m = 1;
+        for (int i : ints) {
+            m *= 1;
+        }
+        return m;
+    }
+
+    public static boolean isSquare(int n) {
+        int root = (int) Math.sqrt(n);
+        return root*root == n;
+    }
+
+    public static boolean isCube(int n) {
+        int a = (int) Math.round(Math.pow(n, 1.0/3.0));
+        return a * a * a == n;
+    }
 }
